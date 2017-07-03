6bbd7c864501c84f9dc9486a6522f9294af83893
Jan Krebs <jan.krebs@meissa-gmbh.de>
2017-07-03 14:57:35 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :profiles {:dev
              {:dependencies
               [[com.palletops/pallet "0.8.12" :classifier "tests"]
-               [ch.qos.logback/logback-classic "1.1.8"]
+               [ch.qos.logback/logback-classic "1.2.3"]
                [com.palletops/pallet-vmfest "0.4.0-alpha.1"]]
               :plugins
               [[com.palletops/pallet-lein "0.8.0-alpha.1"]
@@ -22,14 +22,14 @@
                ;; To get started we include all jclouds compute providers.
                ;; You may wish to replace this with the specific jclouds
                ;; providers you use, to reduce dependency sizes.
-               [org.apache.jclouds/jclouds-allblobstore "2.0.0"]
-               [org.apache.jclouds/jclouds-allcompute "2.0.0"]
-               [org.apache.jclouds.driver/jclouds-slf4j "2.0.0"
+               [org.apache.jclouds/jclouds-allblobstore "2.0.1"]
+               [org.apache.jclouds/jclouds-allcompute "2.0.1"]
+               [org.apache.jclouds.driver/jclouds-slf4j "2.0.1"
                 ;; the declared version is old and can overrule the
                 ;; resolved version
                 :exclusions [org.slf4j/slf4j-api]]
-               [org.apache.jclouds.driver/jclouds-sshj "2.0.0"]]}
+               [org.apache.jclouds.driver/jclouds-sshj "2.0.1"]]}
              :leiningen/reply
-             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.22"]]
+             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.25"]]
               :exclusions [commons-logging]}}
   :local-repo-classpath true)
