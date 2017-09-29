871b98ff2b9e10e4f3ac7a78be1dd028d7f14e4c
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-09-29 11:21:11 +0200

    lein-release plugin: preparing 0.2.7 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/httpd "0.2.7-SNAPSHOT"
+(defproject dda/httpd "0.2.7"
   :description "Pallet crate to install and run Apache httpd"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.palletops/pallet "0.8.12"]
@@ -22,13 +22,13 @@
                ;; To get started we include all jclouds compute providers.
                ;; You may wish to replace this with the specific jclouds
                ;; providers you use, to reduce dependency sizes.
-               [org.apache.jclouds/jclouds-allblobstore "2.0.1"]
-               [org.apache.jclouds/jclouds-allcompute "2.0.1"]
-               [org.apache.jclouds.driver/jclouds-slf4j "2.0.1"
+               [org.apache.jclouds/jclouds-allblobstore "2.0.2"]
+               [org.apache.jclouds/jclouds-allcompute "2.0.2"]
+               [org.apache.jclouds.driver/jclouds-slf4j "2.0.2"
                 ;; the declared version is old and can overrule the
                 ;; resolved version
                 :exclusions [org.slf4j/slf4j-api]]
-               [org.apache.jclouds.driver/jclouds-sshj "2.0.1"]]}
+               [org.apache.jclouds.driver/jclouds-sshj "2.0.2"]]}
              :leiningen/reply
              {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.25"]]
               :exclusions [commons-logging]}}
